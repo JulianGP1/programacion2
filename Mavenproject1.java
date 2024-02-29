c84f0c7d615c5e89b8a4b22af0504ae41caefb0c
Julian <jaguerreropa@ufpso.edu.co>
2024-02-29 15:56:08 -0500

    v0.0.2

---
+++ Mavenproject1.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.mavenproject1;
+
+import people.Character;
+public class Mavenproject1 {
+
+    public static void main(String args[]) {
+        var homero = new Character("homero", 39, 'm', "a la grande le puse cuca");
+        homero.saySomthing();
+    }
+
+  
+
+    }
+
