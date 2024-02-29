bc5c201bed67d5ff55f1aeda25c5f4ebb1853ce9
Julian <jaguerreropa@ufpso.edu.co>
2024-02-29 15:24:25 -0500

    subiendo

--- newJavaFile1.java
+++ newJavaFile1.java
@@ -1,8 +1,28 @@
 
-public class newJavaFile1 {
-
+public class NewJavaFile1 {
 
     public static void main(String args[]) {
-   
+        var homero = new Character("homero", 39, 'm', "a la grande le puse cuca");
+        homero.saySomthing();
+    }
+
+    static class Character {
+
+        String name;
+        int age;
+        char gender;
+        String phrase;
+
+        public Character(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase = phrase;
+        }
+
+        void saySomthing() {
+            System.out.println(this.phrase);
+        }
+
     }
 }
